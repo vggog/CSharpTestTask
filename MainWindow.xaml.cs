c569763d42699b5db764199fd5b43aa785b4950c
vggog <gluhovkich@gmail.com>
2023-12-21 17:30:30 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            WeatherButton.IsEnabled = true;
+
+            if (SityTextBox.Text == "")
+            {
+                WeatherButton.IsEnabled = false;
+            }
+        }
+
+        private void GetWeather(object sender, RoutedEventArgs e)
+        {
+            SityTextBox.Text = "Нажал";
+        }
+    }
+}
